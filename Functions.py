8048630da3333f8fd33df2d844a2a67ec6284e9a
pranav3217 <52619402+pranav3217@users.noreply.github.com>
2019-09-05 08:59:21 +0530

    Create Functions.py

---
+++ Functions.py
@@ -0,0 +1,14 @@
+def Song():
+    name = "Thriller"
+    return print (name)
+Song()
+
+def Artist():
+    name = "Michael Jackson"
+    return print (name)
+Artist()
+
+def YearOfRelease():
+    name = 1984
+    return print (name)
+YearOfRelease()
